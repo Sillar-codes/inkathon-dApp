9883e77242eed35c3509b58616a0037b49342e4c
Christian <sillarcode@gmail.com>
2025-11-12 08:33:30 -0500

    Fix hook errors in frontend

--- frontend/src/hooks/use-chain-meta.tsx
+++ frontend/src/hooks/use-chain-meta.tsx
@@ -24,6 +24,14 @@ export function useChainMeta() {
       const chainId = await api.constants.Revive.ChainId()
       const ss58Prefix = await api.constants.System.SS58Prefix()
       const versions = await api.constants.System.Version()
+
+      setChainMeta({
+        name: chainSpec.name,
+        ss58Prefix: chainSpec.properties?.ss58Format || ss58Prefix,
+        implName: versions.impl_name,
+        systemVersion: versions.system_version,
+        hasPalletRevive: !!chainId,
+      })
     } catch (error) {
       console.error(error)
     } finally {
